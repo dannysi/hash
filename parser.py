3bcb9ce0c2e4a18f3c4d024ec895882c269bd492
Mor Mayr <mor.mayr@nice.com>
2021-02-25 21:29:38 +0200

    Update parser.py

--- parser.py
+++ parser.py
@@ -1,5 +1,3 @@
-
-
 class Street(object):
     def __init__(self, from_int, to_int, name, length):
         self.from_int = from_int
@@ -10,13 +8,6 @@ class Street(object):
     def String(self):
         print("street", self.name, self.from_int, self.to_int, self.length)
 
-class Car(object):
-    def __init__(self, id, route):
-        self.id = id
-        self.route = route
-
-    def String(self):
-        print("car", self.route)
 
 class StopLight(object):
     def __init__(self, s_intersection, s_street, s_cars):
@@ -30,10 +21,13 @@ class StopLight(object):
     def String(self):
         print("intersection", self.s_intersection, "street", self.s_street, "cars", self.s_cars)
 
-class Schedule(object):
-    def __init__(self):
 
+class Car(object):
+    def __init__(self, route):
+        self.route = route
 
+    def String(self):
+        print("car", self.route)
 
 
 class Intersection(object):
@@ -42,14 +36,15 @@ class Intersection(object):
         self.to_street = []
         self.from_street = []
 
-    def add_to(self,street):
+    def add_to(self, street):
         self.to_street.append(street)
 
-    def add_from(self,street):
+    def add_from(self, street):
         self.from_street.append(street)
 
     def String(self):
         print("intersection", self.name, "from:", self.from_street, "to", self.to_street)
+
 
 def read_from_file(file):
     f = open(file, "r")
@@ -61,9 +56,9 @@ def read_from_file(file):
         from_int, to_int, name, length = f.readline().split()
         streets.append(Street(int(from_int), int(to_int), name, int(length)))
 
-    for id in range(int(cars_len)):
+    for _ in range(int(cars_len)):
         _, route = f.readline().split(maxsplit=1)
-        cars.append(Car(id, route.split()))
+        cars.append(Car(route.split()))
     intersections = []
     for i in range(int(intersection_len)):
         intersections.append(Intersection(i))
@@ -71,13 +66,28 @@ def read_from_file(file):
         intersections[street.from_int].add_from(street.name)
         intersections[street.to_int].add_to(street.name)
 
+    stoplights = init_stoplights(streets, cars, intersections)
+
+    return (streets, cars, intersections, score, stoplights)
 
 
-    return (streets, cars, intersections, score)
+def init_stoplights(i_streets, i_cars, i_intersections):
+    t_streets = dict()
+    stoplights = []
+    for car in i_cars:
+        for street_name in car.route:
+            if street_name not in t_streets.keys():
+                t_streets[street_name] = [car]
+            else:
+                t_streets[street_name].append(car)
+
+    for inters in i_intersections:
+        for from_street in inters.from_street:
+            stoplights.append(StopLight(inters, from_street, t_streets[from_street]))
+    return stoplights
 
 
-
-streets, cars, intersections, score = read_from_file("a.txt")
+streets, cars, intersections, score, stoplights = read_from_file("a.txt")
 
 for street in streets:
     street.String()
@@ -86,5 +96,5 @@ for car in cars:
 for intersection in intersections:
     intersection.String()
 
-
-def write_solution():
+for stoplight in stoplights:
+    stoplight.String()
